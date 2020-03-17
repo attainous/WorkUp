6a8266bc969247c3cb01feb80e17d62ffff964e1
aydinyalcinkaya <captaincanada03@gmail.com>
2020-03-17 18:57:05 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron');
+const notifier = require('node-notifier');
+const path = require('path');
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+        resizable: false,
+        width: 600,
+        height: 600,
+        icon: './icon.png',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+    win.removeMenu()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
